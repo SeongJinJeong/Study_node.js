6f7fe4fadf8ac5dce001edcf3944926842b0d97c
JeongSeongJin <jsj0718tjdwl@naver.com>
2018-07-25 12:27:51 +0900

    Added cors,compression,middleware,routing

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+const compression = require('compression');
 const path = require('path');
 const app = express();
+const route = require('./router.js');
+
+app.use('/',route);
+app.use(cors());
+app.use(compression());
+app.use((req,res,next)=>{
+	res.status(404).send('일치하는 주소가 없습니다.');
+	next();
+});
+app.use((err,req,res,next)=>{
+	console.error(err.stack);
+	res.status(500).send('서버에러!');
+})
+
+
 
 app.use(express.static(path.join(__dirname,'html')));
 app.get('/',(req,res)=>{
@@ -10,6 +27,8 @@ app.get('/about',(req,res)=>{
 	res.sendFile(path.join(__dirname,'html','about.html'));
 })
 
+
+
 app.listen(8080,()=>{
-	console.log('Express is Successfully Activated');
+	console.log('Express is Successfully Activated on Port: 8080!');
 });
