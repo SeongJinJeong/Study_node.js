7ba524dcd1376c2c9ef412e608aa4195ef8adc72
JeongSeongJin <jsj0718tjdwl@naver.com>
2018-07-26 12:04:09 +0900

    Changed Syntax, '=>' to 'function()'

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const route = require('./router.js');
 app.use('/',route);
 app.use(cors());
 app.use(compression());
-app.use((req,res,next)=>{
+app.use(function(req,res,next){
 	res.status(404).send('일치하는 주소가 없습니다.');
 	next();
 });
-app.use((err,req,res,next)=>{
+app.use(function(err,req,res,next){
 	console.error(err.stack);
 	res.status(500).send('서버에러!');
 })
@@ -20,15 +20,15 @@ app.use((err,req,res,next)=>{
 
 
 app.use(express.static(path.join(__dirname,'html')));
-app.get('/',(req,res)=>{
+app.get('/',function(req,res){
 	res.sendFile(path.join(__dirname,'html','hello world.html'));
 });
-app.get('/about',(req,res)=>{
+app.get('/about',function(req,res){
 	res.sendFile(path.join(__dirname,'html','about.html'));
 })
 
 
 
-app.listen(8080,()=>{
+app.listen(8080,function(){
 	console.log('Express is Successfully Activated on Port: 8080!');
 });
