0fced9bb06ff40afcdfe0c4550fc3f4c5821de37
JeongSeongJin <jsj0718tjdwl@naver.com>
2018-07-30 09:45:40 +0900

    Downloaded 'body-parser'module,changed method to 'post' from 'get'

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static('html'));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.get('/',function(req,res){
 	res.sendFile(path.join(__dirname,'html','hello world.html'));
 });
+app.post('/login_server',function(req,res){
+	var id = req.body.ID;
+	var passwd = req.body.passwd;
+
+	if(id == "jsj" && passwd == "jsh8689"){
+		res.send('Login Complete!');
+	}
+	else{
+		res.send('Login Denied!');
+	}
+})
 app.get('/login',function(req,res){
 	res.sendFile(path.join(__dirname,'html','login.html'));
 });
