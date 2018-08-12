bd691b2fe92918f2325b843ae99b854edf820695
JeongSeongJin <jsj0718tjdwl@naver.com>
2018-08-12 17:06:57 +0900

    Added redierct opition

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+	host : 'localhost',
+	user : 'root',
+	password : '1234',
+	database : 'o2'
+})
 
 app.set('view engine','jade');
 app.set('views','./views');
@@ -12,20 +19,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/',function(req,res){
 	res.sendFile(path.join(__dirname,'html','hello world.html'));
 });
+
+app.get('/login',function(req,res){
+	res.sendFile(path.join(__dirname,'html','login.html'));
+});
+
 app.post('/login_server',function(req,res){
 	var id = req.body.ID;
 	var passwd = req.body.passwd;
 
 	if(id == "jsj" && passwd == "jsh8689"){
-		res.send('Login Complete!');
+		res.redirect('/');
 	}
 	else{
-		res.send('Login Denied!');
+		res.redierct('/login');
 	}
 })
-app.get('/login',function(req,res){
-	res.sendFile(path.join(__dirname,'html','login.html'));
-});
+
 app.get('/login_server',function(req,res){
 	var id = req.query.ID;
 	var passwd = req.query.passwd;
